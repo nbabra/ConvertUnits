9d4a9d3afe32fc91af7b0994755bfe64793a93ed
Navneeth Babra <nbabra@umd.edu>
2019-01-25 00:06:13 -0500

    fixed errors

--- app.js
+++ app.js
@@ -2,33 +2,33 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('Please use /units/si');
+    res.send('Please use /units/si');
 });
 
 const convert = require('./convert.js');
 
-app.get('/units/si', function(req, res) {
+app.get("/units/si", function(req, res) {
 
-	var unit = req.query.units;
-	var resultMap = new Map();
+    var unit = req.query.units;
+    var resultMap = new Map();
 
     resultMap = convert.evaluateUnits(unit);
 
     var JsonData = {};
 
     JsonData = {
-    			'unit_name':resultMap.get('unit_name'), 
-    			'multiplication_factor':resultMap.get('multiplication_factor'
-    		   )};
+                "unit_name":resultMap.get('unit_name'), 
+                "multiplication_factor":resultMap.get('multiplication_factor')
+                };
 
     res.send(JSON.stringify(JsonData, null, 2));
 
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.info(`Running on Port ${PORT}`));
-
-
+app.listen(PORT, () => {
+    console.log('Listening on port %d', PORT);
+});
 
 
